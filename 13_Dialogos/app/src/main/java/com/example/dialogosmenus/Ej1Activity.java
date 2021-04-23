57d8a6fe6a8aa7b65f24b74d5a7359377ab03a7d
Alejandro Vidal Domínguez <avidaldo@ateis.local>
2021-04-23 20:48:26 +0200

    ejercicio switch corregido

--- 13_Dialogos/app/src/main/java/com/example/dialogosmenus/Ej1Activity.java
+++ 13_Dialogos/app/src/main/java/com/example/dialogosmenus/Ej1Activity.java
@@ -15,10 +15,11 @@ import android.widget.ToggleButton;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 
-public class Ej1Activity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class Ej1Activity extends AppCompatActivity {
 
     TextView tv;
     Switch interruptor;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +28,39 @@ public class Ej1Activity extends AppCompatActivity implements CompoundButton.OnC
 
         tv = findViewById(R.id.textView2);
         interruptor = findViewById(R.id.switch1);
+        et = findViewById(R.id.editTextTextPersonName);
 
-        interruptor.setOnCheckedChangeListener(this);
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (!isChecked) {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-            alertDialogBuilder.setTitle("Atención!");
-            alertDialogBuilder.setMessage(((EditText)findViewById(R.id.editTextTextPersonName)).getText().toString());
-
-            alertDialogBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    tv.setVisibility(View.GONE);
-                }
-            });
-
-            alertDialogBuilder.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialog) {
+        interruptor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( interruptor.isChecked() ) { // Si se pulsa el switch estando a off
                     interruptor.setChecked(true);
+                    tv.setVisibility(View.VISIBLE);
+                } else {
+                    interruptor.setChecked(true);
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(Ej1Activity.this);
+                    dialog.setTitle("Atención!");
+                    dialog.setMessage(et.getText().toString());
+                    dialog.setCancelable(false);
+
+                    dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            interruptor.setChecked(false);
+                            tv.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+                    dialog.create().show();
                 }
-            });
-            alertDialogBuilder.show();
-        }
-        else {
-            tv.setVisibility(View.VISIBLE);
-        }
+            }
+        });
     }
+
+
 
 }
